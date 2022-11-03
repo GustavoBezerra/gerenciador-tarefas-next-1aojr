a896055fa7efb4bd74dfe6a0da9ce7aa1c487cbd
Gustavo Bezerra <gustavobz305@gmail.com>
2022-11-03 20:36:51 -0300

    Adicionando objeto default para response

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,20 +1,27 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
+import { DefaultMessageRespose } from '../../types/DefaultMessageRespose';
 
-export default function(requisicao: NextApiRequest, resposta: NextApiResponse){
+export default function(requisicao: NextApiRequest, resposta: NextApiResponse<DefaultMessageRespose>){
 
-    if(requisicao.method !== 'POST'){
-        return resposta.status(405).json({error: 'Método informado não existe!'});
-    }
-
-    if(!requisicao.body){
-        return resposta.status(400).json({error: 'Favor informar os dados para autenticação'});
-    }
-
-    const {login, password} = requisicao.body;
-
-    if(login === 'teste@teste.com'
-        && password === 'teste@123'){
-            return resposta.status(200).json({msg: 'Usuário autenticado!'});
+    try{
+        if(requisicao.method !== 'POST'){
+            return resposta.status(405).json({error: 'Método informado não existe!'});
         }
-    return resposta.status(400).json({error: 'Usuário e senha não conferem'});
+    
+        if(!requisicao.body){
+            return resposta.status(400).json({error: 'Favor informar os dados para autenticação'});
+        }
+    
+        const {login, password} = requisicao.body;
+    
+        if(login === 'teste@teste.com'
+            && password === 'teste@123'){
+                return resposta.status(200).json({msg: 'Usuário autenticado!'});
+            }
+        return resposta.status(400).json({error: 'Usuário e senha não conferem'});
+    } catch(e : any){
+        console.log('Ocorreu erro ao logar usuário:', e);
+        return resposta.status(500).json({error: 'Ocorreu erro ao logar usuário'});
+    }
+    
 }
---
+++ types/DefaultMessageRespose.ts
@@ -0,0 +1,4 @@
+export type DefaultMessageRespose = {
+    error? : string,
+    msg? : string
+}
