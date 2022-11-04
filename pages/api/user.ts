b4299111cee9233b082573f0cfd8aa1ed247cb96
Gustavo Bezerra <gustavobz305@gmail.com>
2022-11-03 21:50:20 -0300

    Criando operacoes de cadastro e busca de usuário no MongoDB

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,13 +1,20 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import { connectToDB } from '../../middlewares/connectToDB';
+import { UserModel } from '../../models/User';
 import { DefaultMessageRespose } from '../../types/DefaultMessageRespose';
 import { User } from '../../types/User';
+import CryptoJS from "crypto-js";
 
-const endpoint = function(req: NextApiRequest, res: NextApiResponse<DefaultMessageRespose>){
+const endpoint = async (req: NextApiRequest, res: NextApiResponse<DefaultMessageRespose>) => {
 
     try{
         if(req.method !== 'POST'){
             return res.status(405).json({error: 'Método informado não existe!'});
+        }
+
+        const {MY_SECRET_KEY} = process.env;
+        if(!MY_SECRET_KEY){
+            return res.status(500).json({error: 'Não foi informada a secretKey'});
         }
     
         if(!req.body){
@@ -28,7 +35,16 @@ const endpoint = function(req: NextApiRequest, res: NextApiResponse<DefaultMessa
             return res.status(400).json({error: 'Senha não é válida.'});    
         }
 
-        return res.status(400).json({error: 'Usuário cadastrado'});
+        const existsWithSameEmail = await UserModel.find({email: user.email});
+        if(existsWithSameEmail && existsWithSameEmail.length > 0){
+            return res.status(400).json({error: 'E-mail já cadastrado.'});
+        }
+
+        user.password = CryptoJS.AES.encrypt(user.password, MY_SECRET_KEY).toString();
+        
+        await UserModel.create(user);
+
+        return res.status(400).json({msg: 'Usuário cadastrado'});
     } catch(e : any){
         console.log('Ocorreu erro ao cadastrar usuário:', e);
         return res.status(500).json({error: 'Ocorreu erro ao cadastrar usuário, tente novamente...'});
